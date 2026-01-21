f9a30f8e8704623b2e79fab710af142f58b5f3a3
zhumeng <zm_dmg@126.com>
2026-01-21 10:18:40 +0800

    代码全量提交

---
+++ SAA-03ChatModelChatClient/src/test/java/com/dmg/saa03chatmodelchatclient/Saa03ChatModelChatClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmg.saa03chatmodelchatclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Saa03ChatModelChatClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ SAA-04StreamingOutput/src/test/java/com/dmg/saa04streamingoutput/Saa04StreamingOutputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmg.saa04streamingoutput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Saa04StreamingOutputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
